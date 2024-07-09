f3eec9b0f3fc3e21d577bfd43b29e3cc1a18f7da
[Sumed <codewithsumeed.18@gmail.com>
2024-07-09 12:56:50 +0530

    Add Parkinsons_Disease as a regular directory

---
+++ Parkinsons_Disease/ReadMe.txt
@@ -0,0 +1,18 @@
+Welcome, to Parkinson's Disease Predication Miniproject
+you will get all the details about the files present in this folder
+> column.txt                 - all short information about the attributes in the dataset  
+> Parkinsons.csv             - sample dataset
+> Parkinsons_du.csv          - trimmed dataset after data understanding.ipynb
+> test values.csv            - values(input data) for testing the model
+> data-understanding.ipynb   - data cleaning and feature selection
+> parkinsonsDataReport.html  - dataset profile report by pandas profiling
+> EDA-Univariate.ipynb       - analysis of univariate data
+> EDA-Multivariate.ipynb     - analysis of multivariate data
+> pairPlotParkinson.png      - a detailed overview image of the scatterplot 
+> Model-Selection            - Splitting data into train and test, 
+                               Selecting the accurate model among the following models,
+                               Model Training, 
+                               Model Evaluation
+>Parkinsons_Model            - Joblib dumped model file
+>WebApp.ipynb                 - runnig the webApp.py gui file
+>webApp.py                   - code for webApp gui 
---
+++ Parkinsons_Disease/webApp.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import streamlit as st
+import joblib
+import numpy as np
+
+
+model = joblib.load('Parkinsons_Model')
+
+st.title('Parkinson Disease Prediction')
+col1, col2, col3, col4, col5 = st.columns(5)
+
+
+with col1:
+        fo = st.text_input("MDVP: Fo(Hz)")
+        
+with col2:
+        fhi = st.text_input('MDVP: Fhi(Hz)')
+        
+with col3:
+        flo = st.text_input('MDVP: Flo(Hz)')      
+        
+with col4:
+        Jitter_Abs = st.text_input('MDVP: Jitter(Abs)')
+        # Jitter_abs_float = convert_scientific_to_float(Jitter_Abs_str)
+
+with col5:
+        PPQ = st.text_input("MDVP: PPQ")
+        
+with col1:
+        DDP = st.text_input('Jitter: DDP')
+        
+with col2:
+        Shimmer = st.text_input('MDVP: Shimmer')       
+        
+with col3:
+        APQ3 = st.text_input('Shimmer: APQ3')
+        
+with col4:
+        APQ5 = st.text_input('Shimmer: APQ5')
+        
+with col5:
+        APQ = st.text_input('MDVP: APQ')
+        
+with col1:
+        NHR = st.text_input('NHR')
+        
+with col2:
+        HNR = st.text_input('HNR')
+        
+with col3:
+        RPDE = st.text_input('RPDE')
+        
+with col4:
+        DFA = st.text_input('DFA')
+        
+with col5:
+        spread1 = st.text_input('spread1')
+        
+with col1:
+        spread2 = st.text_input('spread2')
+        
+with col2:
+        D2 = st.text_input('D2')
+        
+
+# code for Prediction
+parkinsons_diagnosis = ''
+    
+# creating a button for Prediction    
+if st.button("Parkinson's Test Result"):
+    features = [fo, fhi, flo, Jitter_Abs, PPQ,DDP,Shimmer,APQ3,APQ5,APQ,NHR,HNR,RPDE,DFA,spread1,spread2,D2]
+    input_data = np.array(features).reshape(1,-1)
+    parkinsons_prediction = model.predict(input_data)                          
+
+    if (parkinsons_prediction[0] == 1):
+        parkinsons_diagnosis = "The person has Parkinson's disease"
+    else:
+        parkinsons_diagnosis = "The person does not have Parkinson's disease"
+
+st.success(parkinsons_diagnosis)
